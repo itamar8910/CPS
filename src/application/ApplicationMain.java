4ca30adee14adfa499a1442e7be88b4074a92103
Itamar <itamar8910@gmail.com>
2018-01-06 11:00:00 +0200

    add app main

---
+++ src/application/ApplicationMain.java
@@ -0,0 +1,137 @@
+package application;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+
+import client.Client;
+import common.ControllerIF;
+import common.Params;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+
+public class ApplicationMain extends Application {
+	Stage primaryStage;
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		// constructing our scene
+//		try {
+			this.setScene("Welcome.fxml", Params.getEmptyInstance());
+//			this.primaryStage = primaryStage;
+//			URL url = getClass().getResource("Welcome.fxml");
+//
+//
+//			FXMLLoader fxmlLoader = new FXMLLoader(url);
+//
+//			AnchorPane pane = (AnchorPane)fxmlLoader.load();
+//			WelcomeController controller = fxmlLoader.<WelcomeController>getController();
+//
+//			List<String> args = getParameters().getRaw();
+//			String host = "localhost";
+//			int port = 5555;
+//			if(args.size() >= 2) {
+//				host = args.get(0);
+//				port = Integer.parseInt(args.get(1));
+//			}
+//			controller.init(this, host, port);
+//
+//			//AnchorPane pane = FXMLLoader.load( url );
+//			Scene scene = new Scene( pane );
+//
+//			// setting the stage
+//			primaryStage.setScene( scene );
+//			primaryStage.setTitle( "Main view" );
+//			primaryStage.show();
+
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+
+	}
+
+	public void setScene(String urlStr, Params params){
+		// constructing our scene
+		try {
+
+			URL url = getClass().getResource(urlStr);
+
+
+			FXMLLoader fxmlLoader = new FXMLLoader(url);
+
+			AnchorPane pane = (AnchorPane)fxmlLoader.load();
+			ControllerIF controller = fxmlLoader.<ControllerIF>getController();
+
+
+
+			List<String> args = getParameters().getRaw();
+			String host = "localhost";
+			int port = 5555;
+			if(args.size() >= 2) {
+				host = args.get(0);
+				port = Integer.parseInt(args.get(1));
+			}
+			controller.init(this, params);
+
+			//AnchorPane pane = FXMLLoader.load( url );
+			Scene scene = new Scene( pane );
+
+			// setting the stage
+			primaryStage.setScene( scene );
+			primaryStage.setTitle( "Main view" );
+			primaryStage.show();
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+//	public void setScene(String url){
+//		// constructing our scene
+//		try {
+//
+//			URL url = getClass().getResource("Welcome.fxml");
+//
+//
+//			FXMLLoader fxmlLoader = new FXMLLoader(url);
+//
+//			AnchorPane pane = (AnchorPane)fxmlLoader.load();
+//			WelcomeController controller = fxmlLoader.<WelcomeController>getController();
+//
+//			List<String> args = getParameters().getRaw();
+//			String host = "localhost";
+//			int port = 5555;
+//			if(args.size() >= 2) {
+//				host = args.get(0);
+//				port = Integer.parseInt(args.get(1));
+//			}
+//			controller.initConnection(host, port);
+//
+//			//AnchorPane pane = FXMLLoader.load( url );
+//			Scene scene = new Scene( pane );
+//
+//			// setting the stage
+//			primaryStage.setScene( scene );
+//			primaryStage.setTitle( "Main view" );
+//			primaryStage.show();
+//
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+
+
+}
