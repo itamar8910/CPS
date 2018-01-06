25bcaf93e189ff2fd59e2c88c0470901956b3ec5
Itamar <itamar8910@gmail.com>
2018-01-06 10:45:14 +0200

    add controller IF and params obj

---
+++ src/common/ControllerIF.java
@@ -0,0 +1,7 @@
+package common;
+
+public interface ControllerIF {
+
+	void init(Params params);
+
+}
---
+++ src/common/Params.java
@@ -0,0 +1,38 @@
+package common;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Params {
+
+	private JSONObject data;
+
+	public Params(String jsonStr){
+		try {
+			data = new JSONObject(jsonStr);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public String getParam(String p){
+		try {
+			return data.getString(p);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "";
+	}
+
+	public void addParam(String p, String val){
+		try {
+			data.put(p, val);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
