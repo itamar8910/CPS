24e61acb579703d28a58b5f6e03f6ea4afd6c814
alon melamud <alonmem@gmail.com>
2018-01-17 21:51:41 +0200

    add preformance report for facility manager

--- application/FacilityMainController.java
+++ application/FacilityMainController.java
@@ -13,6 +13,7 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class FacilityMainController implements ControllerIF{
@@ -20,9 +21,16 @@ public class FacilityMainController implements ControllerIF{
     private ApplicationMain main;
     private Params params;
     private JSONArray curList;
+    private String parkingName;
 	
     @FXML
     private ResourceBundle resources;
+    
+    @FXML
+    private Text parkingNameText;
+
+    @FXML
+    private Button perfReportButton;
 
     @FXML
     private URL location;
@@ -142,18 +150,56 @@ public class FacilityMainController implements ControllerIF{
     	}
 
     @FXML
+    void performanceClicked(ActionEvent event) {
+    	Params serverRequest = Params.getEmptyInstance();
+    	serverRequest.addParam("action", "getSubscriptionStats");
+    	serverRequest.addParam("name", parkingName);
+    	
+    	final Params temp = Params.getEmptyInstance();
+
+    	// send this JSON to server
+    	TalkToServer.getInstance().send(serverRequest.toString(), msg -> {
+    		Params res = new Params(msg);
+
+    		if(true || res.getParam("status").equals("OK")){ 
+    			Platform.runLater(new Runnable() {
+    	  		      @Override public void run() {
+    	  	    		final Stage dialog = new Stage();
+    	  	    		temp.addParam("monthly", res.getParam("monthly").toString());
+    	  	    		temp.addParam("monthlyWithMoreCars", res.getParam("monthlyWithMoreCars").toString());
+    	  	    		
+    	  	 			PDFstatus pdf = new PDFstatus();
+       	  	 	    	pdf.createCurrentReport(res.getParam("name").toString(),temp);
+
+    	  		      }});
+    			}});  
+    }
+    
+    @FXML
     void initialize() {
         assert RateChangeButton != null : "fx:id=\"RateChangeButton\" was not injected: check your FXML file 'FacilityManagerMain.fxml'.";
         assert complainsReportButton != null : "fx:id=\"complainsReportButton\" was not injected: check your FXML file 'FacilityManagerMain.fxml'.";
         assert ordersReportButton != null : "fx:id=\"ordersReportButton\" was not injected: check your FXML file 'FacilityManagerMain.fxml'.";
         assert parkingDownReport != null : "fx:id=\"parkingDownReport\" was not injected: check your FXML file 'FacilityManagerMain.fxml'.";
-
+        assert parkingNameText != null : "fx:id=\"parkingNameText\" was not injected: check your FXML file 'FacilityManagerMain.fxml'.";
+        assert perfReportButton != null : "fx:id=\"perfReportButton\" was not injected: check your FXML file 'FacilityManagerMain.fxml'.";
     }
     
 	@Override
 	public void init(ApplicationMain main, Params params) {
 		this.main = main;
 		this.params = params;
+				
+		Params serverRequest = Params.getEmptyInstance();
+    	serverRequest.addParam("action", "requestParkingStatusData");
+    	serverRequest.addParam("facID", this.params.getParam("facID").toString());
+    	// send this JSON to server
+    	TalkToServer.getInstance().send(serverRequest.toString(), msg -> {
+    		Params res = new Params(msg);
+    		parkingName = res.getParam("name").toString();
+        	parkingNameText.setText(parkingName);
+    	});  
+    	
 	}
 
 
